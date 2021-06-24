7b6db45cd8a1137bb7db9ab7888a953d8caf63f9
marcio <ciorma7@hotmail.com>
2021-06-23 21:10:37 -0300

    Criação do Swagger

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 import { router } from './routes'
+import swaggerUi from 'swagger-ui-express'
 
+const swaggerFile = require('../swagger_output.json')
 const app = express()
 
 app.use(express.json())
 app.use(router)
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 export { app }
